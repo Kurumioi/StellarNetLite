0cb54809f8e2dab51d749f8805829d0334a04f2b
DESKTOP-R3CF6V4\Administrator <18870152193@163.com>
2026-04-12 02:23:22 +0800

    补充自动生成占位代码

--- StellarNetLiteGenerated/MsgIdConst.cs
+++ StellarNetLiteGenerated/MsgIdConst.cs
@@ -2,6 +2,7 @@ namespace StellarNet.Lite.Shared.Protocol
 {
     public static class MsgIdConst
     {
+        public const int C2S_Ping = 10;
         public const int C2S_Login = 100;
         public const int C2S_ConfirmReconnect = 103;
         public const int C2S_ReconnectReady = 105;
